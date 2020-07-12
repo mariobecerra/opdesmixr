185671874073e99e6f3cd2bf29615d210a05cf59
Mario Becerra <mbecerracontreras@gmail.com>
2020-07-12 08:55:40 +0200

    Separate efficiency plots in two scripts because ggtern does something to ggplot2

--- tests/i_optimality_vs_d_optimality/relative_efficiencies_designs.R
+++ tests/i_optimality_vs_d_optimality/relative_efficiencies_designs.R
@@ -14,7 +14,7 @@ out_dir_1 = "tests/i_optimality_vs_d_optimality/out/"
 out_dir_2 = paste0(out_dir_1, "rds_files/")
 out_dir = paste0(out_dir_2, "many_random_designs/")
 
-file_names = list.files(out_dir)
+file_names = list.files(here(out_dir))
 
 
 efficiencies = map_df(seq_along(file_names), function(i){
@@ -66,76 +66,5 @@ efficiencies = map_df(seq_along(file_names), function(i){
   )
 
 
-
-efficiencies %>%
-  group_by(q, J, S) %>%
-  summarize(
-    n = n(),
-    sum_d_eff_diff = sum(d_eff_of_d_optimal_design < d_eff_of_i_optimal_design),
-    sum_i_eff_diff = sum(i_eff_of_i_optimal_design < i_eff_of_d_optimal_design)) %>%
-  mutate(
-    group = paste0("q =", q, ", J =", J, ", S =", S),
-    perc_d_eff_diff = sum_d_eff_diff/n,
-    perc_i_eff_diff = sum_i_eff_diff/n
-  ) %>%
-  pivot_longer(cols = starts_with("perc")) %>%
-  ggplot() +
-  geom_bar(aes(name, value), stat = "identity") +
-  facet_wrap(~group) +
-  xlab("") +
-  ylab("Percentage") +
-  geom_hline(yintercept = 1)
-
-
-
-# I like the boxplot better
-efficiencies %>%
-  mutate(group = paste0("q = ", q, ", J = ", J, ", S = ", S)) %>%
-  ggplot() +
-  geom_histogram(aes(ratio_d_eff)) +
-  facet_wrap(~group, scales = "free")
-
-
-efficiencies %>%
-  filter(complete.cases(.)) %>%
-  mutate(group = paste0("q = ", q, ", J = ", J, ", S = ", S)) %>%
-  ggplot() +
-  geom_boxplot(aes(group, ratio_d_eff)) +
-  geom_hline(yintercept = 1, linetype = "dotted") +
-  ggplot2::coord_flip() +
-  xlab("") +
-  ylab("Ratio of D-efficiencies")
-
-
-
-efficiencies %>%
-  mutate(group = paste0("q = ", q, ", J = ", J, ", S = ", S)) %>%
-  ggplot() +
-  geom_boxplot(aes(group, ratio_i_eff)) +
-  geom_hline(yintercept = 1, linetype = "dotted") +
-  coord_flip() +
-  xlab("") +
-  ylab("Ratio of I-efficiencies")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+saveRDS(efficiencies, here(out_dir_2, "efficiencies_all.rds"))
 
